e3296d6c60e239c9783956e449172e82abd9f2ac
Sofia <ascto1@gmail.com>
2019-05-13 15:19:48 +0100

    updt comp

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import List from './pages/List';
+import Staff from './pages/Staff';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import api from '../api';
 import MainNavbar from './MainNavbar'
+import Gallery from './pages/Gallery';
 
 export default class App extends Component {
   constructor(props) {
@@ -25,7 +26,8 @@ export default class App extends Component {
         <MainNavbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/list" component={List} />
+          <Route path="/staff" component={Staff} />
+          <Route path="/gallery" component={Gallery} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
           <Route render={() => <h2>404</h2>} />
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -9,9 +9,7 @@ export default class Home extends Component {
   render() {                
     return (
       <div className="Home">
-        <h2>Welcome!</h2>
-        <p>This website is an example of a project using the MERN stack.</p>
-        <p>It was designed for street arts lovers! You can find and add street arts all around the world!</p>
+        <h2>Welcome to Barbearia Du'Arte!</h2>
         <p>Have fun <span role="img" aria-label="smile">😀</span></p>
       </div>
     );
