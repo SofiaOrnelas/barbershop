44186e63c1045931e58b6b73532234f95f82441b
Nelson Aires <nelsonaires@Nelsons-MacBook-Pro.local>
2019-05-13 17:27:48 +0100

    1

--- client/src/components/pages/Footer.jsx
+++ client/src/components/pages/Footer.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer1">
+    <div className="footer1">
     
-      <ul class>
-        <a href="https://www.facebook.com" target="blank"><img src="../Images/icons/facebook.svg" width="55"
+      <ul>
+        <a href="https://www.facebook.com" target="blank"><img src="/icons/instagram.svg" width="55" height="55"
             alt="facebook.svg"></img></a>
         <a href="https://www.linkedin.com" target="blank"><img src="./../../../../Images/icons/linkedin.svg" width="55"
             alt="twitter.png"></img></a>
@@ -15,9 +15,7 @@ export default class Footer extends Component {
         <a href="https://www.pinterest.com" target="blank"><img src="./../../../../Images/icons/pinterest.svg" width="55"
             alt="pinterest.png"></img></a>
       </ul>
-
-        Footer fofo
-      </div>
+    </div>
     )
   }
 }
