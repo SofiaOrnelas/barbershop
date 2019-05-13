1c104fc2f836446b338fdcf89415b2c7bf9598bc
Hugo <hugomaneira@gmail.com>
2019-05-13 18:02:51 +0100

    calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -3,30 +3,66 @@ import Timeline from 'react-calendar-timeline'
 import 'react-calendar-timeline/lib/Timeline.css'
 import moment from 'moment'
 
-const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
- 
+const groups = [{ id: 1, title: 'Duarte' }, { id: 2, title: 'Rafa' }, { id: 3, title: 'Di' }, { id: 4, title: 'Marinho' }, { id: 5, title: 'Bau' }]
+const minTime = moment().add(0, 'hours').valueOf()
+const maxTime = moment().add(10, 'days').valueOf()
 const items = [
   {
     id: 1,
     group: 1,
-    title: 'item 1',
-    start_time: moment(),
-    end_time: moment().add(1, 'hour')
+    title: 'staff 1',
+    // stackItems: true,
+    // height: 30,
+    canChangeGroup: true,
+    canMove: true,
+    style: {
+      backgroundColor: 'black'
+    },
+    // sidebarWidth: 200,
+    traditionalZoom: true,
+    color: 'rgb(158, 14, 6) !important',
+    selectedBgColor: 'rgba(225, 166, 244, 1)',
+    bgColor : 'rgba(225, 166, 244, 0.6)',
+    // itemTouchSendsClick: true,
+    start_time: moment().add(0, 'hour'),    
+    end_time: moment().add(1, 'hour'),
+    timeSteps: {
+      minute: 30,
+      // hour: 1,
+    }
   },
   {
     id: 2,
     group: 2,
-    title: 'item 2',
-    start_time: moment().add(-0.5, 'hour'),
-    end_time: moment().add(0.5, 'hour')
-  },
+    title: 'staff 2',
+    canChangeGroup: true,
+    start_time: moment().add(-1.21, 'hour'),    
+    end_time: moment().add(-0.27, 'hour')
+    },
   {
     id: 3,
-    group: 1,
-    title: 'item 3',
-    start_time: moment().add(2, 'hour'),
-    end_time: moment().add(3, 'hour')
-  }
+    group: 3,
+    title: 'staff 3',
+    canChangeGroup: true,
+    start_time: moment().add(-1.21, 'hour'),    
+    end_time: moment().add(-0.27, 'hour')
+    },
+  {
+    id: 4,
+    group: 4,
+    title: 'staff 4',
+    canChangeGroup: true,
+    start_time: moment().add(-1.21, 'hour'),    
+    end_time: moment().add(-0.27, 'hour')
+    },
+  {
+    id: 5,
+    group: 5,
+    title: 'staff 5',
+    canChangeGroup: true,
+    start_time: moment().add(-1.21, 'hour'),    
+    end_time: moment().add(-0.27, 'hour')
+    },
 ]
 
 export default class App extends Component {
@@ -34,6 +70,18 @@ export default class App extends Component {
     super(props)
     this.state = {
       dates: new Date(),
+    }
+  }
+
+  function (visibleTimeStart, visibleTimeEnd, updateScroll) {
+    if (visibleTimeStart < minTime && visibleTimeEnd > maxTime) {
+      updateScroll(minTime, maxTime)
+    } else if (visibleTimeStart < minTime) {
+      updateScroll(minTime, minTime + (visibleTimeEnd - visibleTimeStart))
+    } else if (visibleTimeEnd > maxTime) {
+      updateScroll(maxTime - (visibleTimeEnd - visibleTimeStart), maxTime)
+    } else {
+      updateScroll(visibleTimeStart, visibleTimeEnd)
     }
   }
 
@@ -45,8 +93,8 @@ export default class App extends Component {
         <Timeline
           groups={groups}
           items={items}
-          defaultTimeStart={moment().add(-12, 'hour')}
-          defaultTimeEnd={moment().add(12, 'hour')}
+          defaultTimeStart={moment().add(-1, 'hour')}
+          defaultTimeEnd={moment().add(6, 'hour')}
         />
       </div>
     );
