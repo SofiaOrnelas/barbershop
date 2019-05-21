f909045ffd15e3eedd6cc13c03c1d5258d20d3f3
Sofia <ascto1@gmail.com>
2019-05-21 19:41:03 +0100

    tst

--- client/src/components/pages/Myprofile.jsx
+++ client/src/components/pages/Myprofile.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class Myprofile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        name: this.state.name, 
+        phone: this.state.phone,
+        email: this.state.email, 
+        bookings: this.state.bookings,
+    }
+  }
   render() {
     return (
       <div>
-          Name: ...   <br />
+          Name: ...  <br />
           Phone: ... <br/>
           Email: ... <br/>
           Bookings: ... <br/>
