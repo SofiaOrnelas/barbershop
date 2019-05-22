57b9a9d636692c979ada81749a3dfcdbe2285dd0
Nelson Aires <nelsonaires@Nelsons-MBP.lan>
2019-05-22 03:30:50 +0100

    eh lah

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,10 +6,10 @@ export default class Footer extends Component {
     <div className="footer1">
     
       <ul className="icons">
-        <a href="https://www.facebook.com" target="blank"><img src="/facebookBr-01.png" width="55" alt="facebook.svg"></img></a>
+        <a href="https://pt-pt.facebook.com/pages/category/Hair-Salon/Barbearia-DuArte-1720241048207314/" target="blank"><img src="/facebookBr-01.png" width="55" alt="facebook.svg"></img></a>
         <a href="https://www.linkedin.com" target="blank"><img src="/linkedinBr-01.png" width="55"
             alt="twitter.png"></img></a>
-        <a href="https://www.instagram.com" target="blank"><img src="/instagramBr-01.png" width="55"
+        <a href="https://theinsta-stalker.com/instagram/barbeariaduarteavb" target="blank"><img src="/instagramBr-01.png" width="55"
             alt="insta.png"></img></a>
         <a href="https://www.pinterest.com" target="blank"><img src="/pinterestBr-01.png" width="55"
             alt="pinterest.png"></img></a>
--- client/src/components/pages/StaffPage.jsx
+++ client/src/components/pages/StaffPage.jsx
@@ -4,72 +4,165 @@ export default class StaffPage extends Component {
   render() {
     return (
    <div className="StaffPage">
+      {/* -----------------  BARBER 1 ------------------------------- */}
       <div className="Barber-Staff-Div">
          <div className="Photo-Staff">
-            <p>Photo</p>
+           <p className="Names">Duarte Fialho</p>
+            <img src="/Staff/1_DU.png" alt="Duarte"/>
          </div>
-         <div className="Slider-Staff">
-            <p>Slider</p>
+       {/* -----------------  SLIDER BARBER 1 ------------------------------- */}
+      <div className="Slider-Staff">        
+         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner-staff">
+               <div className="carousel-item active" data-interval="2000">
+                 <img src="/SliderStaff/DU_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_1.jpg"/>
+               </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DU_SLIDER-02.png" className="d-block mw-100 mh-100" alt="slider_2.jpg"/>
+                  </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DU_SLIDER-03.png" className="d-block mw-100 mh-100" alt="slider_3.jpg"/>
+                  </div>
+            </div>
+               <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Previous</span>
+               </a>
+               <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Next</span>
+               </a>
+            </div>  
          </div>
       </div>
-
+          {/* -----------------  BARBER 2 ------------------------------- */}
       <div className="Barber-Staff-Div">
          <div className="Photo-Staff">
-            <p>Photo</p>
+         <p className="Names">Rafael Rodrigues</p>
+            <img src="/Staff/2_RAFA.png" alt="Rafa"/>
          </div>
-         <div className="Slider-Staff">
-            <p>Slider</p>
+
+          {/* -----------------  SLIDER BARBER 2 ------------------------------- */}
+      <div className="Slider-Staff">        
+         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner-staff">
+               <div className="carousel-item active" data-interval="3000">
+                 <img src="/SliderStaff/RAFA_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_1.jpg"/>
+               </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/RAFA_SLIDER-02.png" className="d-block mw-100 mh-100" alt="slider_2.jpg"/>
+                  </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/RAFA_SLIDER-03.png" className="d-block mw-100 mh-100" alt="slider_3.jpg"/>
+                  </div>
+            </div>
+               <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Previous</span>
+               </a>
+               <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Next</span>
+               </a>
+            </div>  
          </div>
       </div>
-
+          {/* -----------------  BARBER 3 ------------------------------- */}
       <div className="Barber-Staff-Div">
          <div className="Photo-Staff">
-            <p>Photo</p>
+         <p className="Names">Di Pinto</p>
+            <img src="/Staff/3_DIP.png" alt="Di"/>
          </div>
-         <div className="Slider-Staff">
-            <p>Slider</p>
+
+          {/* -----------------  SLIDER BARBER 3 ------------------------------- */}
+      <div className="Slider-Staff">        
+         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner-staff">
+               <div className="carousel-item active" data-interval="4000">
+                 <img src="/SliderStaff/DIP_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_1.jpg"/>
+               </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DIP_SLIDER-02.png" className="d-block mw-100 mh-100" alt="slider_2.jpg"/>
+                  </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DIP_SLIDER-03.png" className="d-block mw-100 mh-100" alt="slider_3.jpg"/>
+                  </div>
+            </div>
+               <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Previous</span>
+               </a>
+               <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Next</span>
+               </a>
+            </div>  
          </div>
       </div>
-
+          {/* -----------------  BARBER 4 ------------------------------- */}
       <div className="Barber-Staff-Div">
          <div className="Photo-Staff">
-            <p>Photo</p>
+         <p className="Names">Marinho</p>
+            <img src="/Staff/4_MAR.png" alt="Marinho"/>
          </div>
-         <div className="Slider-Staff">
-            <p>Slider</p>
+
+          {/* -----------------  SLIDER BARBER 4 ------------------------------- */}
+      <div className="Slider-Staff">        
+         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner-staff">
+               <div className="carousel-item active" data-interval="5000">
+                 <img src="/SliderStaff/MARIO_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_1.jpg"/>
+               </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/MARIO_SLIDER-02.png" className="d-block mw-100 mh-100" alt="slider_2.jpg"/>
+                  </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/MARIO_SLIDER-03.png" className="d-block mw-100 mh-100" alt="slider_3.jpg"/>
+                  </div>
+            </div>
+               <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Previous</span>
+               </a>
+               <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Next</span>
+               </a>
+            </div>  
          </div>
       </div>
-
+          {/* -----------------  BARBER 5 ------------------------------- */}
       <div className="Barber-Staff-Div">
          <div className="Photo-Staff">
-            <p>Photo</p>
+         <p className="Names">Bau</p>
+            <img src="/Staff/5_BAU.png" alt="Bau"/>
          </div>
-         <div className="Slider-Staff">
-            <p>Slider</p>
+
+          {/* -----------------  SLIDER BARBER 5 ------------------------------- */}
+      <div className="Slider-Staff">        
+         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner-staff">
+               <div className="carousel-item active" data-interval="6000">
+                 <img src="/SliderStaff/DU_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_1.jpg"/>
+               </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DU_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_2.jpg"/>
+                  </div>
+                  <div className="carousel-item" data-interval="2000">
+                    <img src="/SliderStaff/DU_SLIDER-01.png" className="d-block mw-100 mh-100" alt="slider_3.jpg"/>
+                  </div>
+            </div>
+               <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Previous</span>
+               </a>
+               <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span className="sr-only">Next</span>
+               </a>
+            </div>  
          </div>
       </div>
-
-
-
-
-
-{/* 
-           <p>Photo</p>
-         </div>
-           <p>Imagem 1</p>
-        </div>
-        <div className="BarberStaff">
-           <p>Imagem 2</p>
-        </div>
-        <div className="BarberStaff">
-           <p>Imagem 3</p>
-        </div>
-        <div className="BarberStaff">
-           <p>Imagem 4</p>
-        </div>
-        <div className="BarberStaff">
-           <p>Imagem 5</p>
-        </div> */}
        
       </div>
     )
