4fda8d4093029d38536f61811278b33a1f31d853
Hugo <hugomaneira@gmail.com>
2019-05-24 11:41:50 +0100

    final

--- client/src/components/pages/Contacts.jsx
+++ client/src/components/pages/Contacts.jsx
@@ -6,14 +6,14 @@ export default class Contacts extends Component {
     return (
     <div className="mainContacts" id="scroll">
       <div className="mainContacts-txt">
-        <p className="contact"><h4>Morada:</h4>
+        <p className="contact"><h4>Address:</h4>
           <h5>Rua Maria Alda Barbosa Nogueira n.4B, Amadora</h5></p>
 
-        <p className="contact"><h4>Segunda a Sábado:</h4>
+        <p className="contact"><h4>Monday to Saturday:</h4>
         <h5>9h-13h / 15h-19h</h5></p>
 
-        <p className="contact"><h4>Domingo e Feriados:</h4>
-        <h5>Descanso</h5></p>
+        <p className="contact"><h4>Sunday and holidays:</h4>
+        <h5>Closed</h5></p>
       </div>
 
       <div className="mainContacts-map">
