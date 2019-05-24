9f24117138ecd1731b43944c4f5a4da26e2cd08a
Sofia <ascto1@gmail.com>
2019-05-24 11:04:00 +0100

    map-insta

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -27,7 +27,7 @@ export default class Footer extends Component {
         <a href="https://pt-pt.facebook.com/pages/category/Hair-Salon/Barbearia-DuArte-1720241048207314/" target="blank"><img src="/facebookBr-01.png" width="55" alt="facebook.svg"></img></a>
         <a href="https://www.linkedin.com" target="blank"><img src="/linkedinBr-01.png" width="55"
             alt="twitter.png"></img></a>
-        <a href="https://theinsta-stalker.com/instagram/barbeariaduarteavb" target="blank"><img src="/instagramBr-01.png" width="55"
+        <a href="https://instagram.com/barbeariaduarteavb?igshid=slapdjts4lqt" target="blank"><img src="/instagramBr-01.png" width="55"
             alt="insta.png"></img></a>
         <a href="https://www.pinterest.com" target="blank"><img src="/pinterestBr-01.png" width="55"
             alt="pinterest.png"></img></a>
